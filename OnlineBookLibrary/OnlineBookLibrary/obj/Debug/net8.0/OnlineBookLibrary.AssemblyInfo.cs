4d711ba930decc95d093daadcd513ed035fea64d
Manjunath <plmanjunath313@gmail.com>
2025-06-10 16:49:34 +0530

    code changes

--- OnlineBookLibrary/OnlineBookLibrary/obj/Debug/net8.0/OnlineBookLibrary.AssemblyInfo.cs
+++ OnlineBookLibrary/OnlineBookLibrary/obj/Debug/net8.0/OnlineBookLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineBookLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+681212fd2660ec8947ca79220cad98e44ac6848e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineBookLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineBookLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
